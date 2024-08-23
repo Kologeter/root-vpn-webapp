eab8ac8cd95a951298269bc903c192dc251a3763
Lukin <mozg.bah@gmail.com>
2024-08-23 21:26:58 +0300

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import {useEffect, useState} from 'react';
 import { Player } from '@lottiefiles/react-lottie-player';
-import duckAnimation from './assets/AnimatedSticker';
+import duckAnimation from './assets/AnimatedSticker.json';
 import axios from 'axios';
 import './App.css';
 

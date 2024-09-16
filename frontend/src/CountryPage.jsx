9deb9ed0a57b2d733a19c510004afa5a321ba6eb
Lukin <mozg.bah@gmail.com>
2024-09-16 16:08:48 +0300

    fix

--- frontend/src/CountryPage.jsx
+++ frontend/src/CountryPage.jsx
@@ -7,32 +7,10 @@ import finlandFlag from './assets/images/finland.jpg';
 import bulgariaFlag from './assets/images/Flag_of_Bulgaria.svg';
 
 function CountryPage() {
-
-    // const [countries, setCountries] = useState([]);
-
-    // Функция для получения данных из POST-запроса
-    // const fetchData = async () => {
-    //     try {
-    //         const response = await fetch('your-api-endpoint', {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //             },
-    //             body: JSON.stringify({ /* любые данные, если нужно */ }),
-    //         });
-    //         const result = await response.json();
-    //         // Предполагаем, что результат будет объектом: { 'Finland', 'Bulgaria', 'Finland 2' }
-    //         setCountries(result); // Сохраняем страны в состояние
-    //     } catch (error) {
-    //         console.error('Ошибка при получении данных:', error);
-    //     }
-    // };
-
-    const site = 'https://test.root-vpn.ru'
-
+    const site = 'https://test.root-vpn.ru';
     const [isSuccess, setIsSuccess] = useState(null);
-    const [hasError, setHasError] = useState(false);  // Для отслеживания ошибок
-    const [selectedCountry, setSelectedCountry] = useState(''); // Сохраняем выбранную страну
+    // const [hasError, setHasError] = useState(false);
+    const [selectedCountry, setSelectedCountry] = useState('');
 
     const sendCountry = (country) => {
         const telegram = window.Telegram.WebApp;
@@ -45,38 +23,21 @@ function CountryPage() {
 
         axios.post(`${site}/changecountry`, { 'user_id': user.id, 'country': country })
             .then((response) => {
-                console.log('response ', response)
-                setIsSuccess(null);
-                setHasError(false);
                 if (response.status === 200) {
                     setIsSuccess(true);
-                    setHasError(false);
+                    // setHasError(false);
                     setSelectedCountry(country);
                 } else {
                     setIsSuccess(false);
-                    setHasError(true);
+                    // setHasError(true);
                 }
             })
             .catch((error) => {
                 console.error('Error:', error);
                 setIsSuccess(false);
-                setHasError(true);
+                // setHasError(true);
             });
-
-        // return (
-        //     <div>
-        //         {isSuccess ? (
-        //             <div className="success-container">
-        //                 <h1 className="change-country-succes">Ваша страна успешно изменена на {country}</h1>
-        //             </div>
-        //         ) : (
-        //             <div className="error-container">
-        //                 <h1 className="change-country-error">Ошибка при смене страны</h1>
-        //             </div>
-        //         )}
-        //     </div>
-        // );
-    }
+    };
 
     return (
         <div className="change-country-container">
@@ -94,15 +55,15 @@ function CountryPage() {
                     Финляндия (резерв)
                 </button>
             </main>
+
             {/* Рендеринг сообщения в зависимости от успешности запроса */}
-            {isSuccess && (
+            {isSuccess === true && (
                 <div className="alert alert-success">
                     <span>Ваша страна успешно изменена на {selectedCountry}</span>
                 </div>
             )}
 
-            {/* Всплывающее окно об ошибке */}
-            {hasError && (
+            {isSuccess === false && (
                 <div className="alert alert-error">
                     <span>Ошибка при смене страны</span>
                 </div>

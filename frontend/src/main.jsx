cf006dbc252ded18da8b4139e1dc6d6db9821a0c
Lukin <mozg.bah@gmail.com>
2024-11-18 01:48:41 +0300

    new vless protocol

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import MainRouter from './MainRouter';
 import './index.css';
-import { CssBaseline } from "@mui/material";
+import CssBaseline from '@mui/material/CssBaseline';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

16cc28b8fa8255228825d79f1a8ce03668d06865
Lukin <mozg.bah@gmail.com>
2024-11-18 03:25:08 +0300

    new vless protocol

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,12 +2,17 @@ import ReactDOM from 'react-dom/client';
 import React from 'react';
 import MainRouter from './MainRouter';
 import './index.css';
-import CssBaseline from '@mui/material/CssBaseline';
+// import CssBaseline from '@mui/material/CssBaseline';
+import ScopedCssBaseline from '@mui/material/ScopedCssBaseline';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <CssBaseline />
-        <MainRouter />
+        <ScopedCssBaseline>
+            <MainRouter />
+        </ScopedCssBaseline>
+        {/*<CssBaseline />*/}
+        {/*<MainRouter />*/}
     </React.StrictMode>
 );

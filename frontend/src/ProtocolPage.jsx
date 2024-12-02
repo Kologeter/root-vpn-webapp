bb3935aa6b4977cd7d6643fcd09c8570b81db4fb
Lukin <mozg.bah@gmail.com>
2024-12-02 05:52:20 +0300

    fix info server

--- frontend/src/ProtocolPage.jsx
+++ frontend/src/ProtocolPage.jsx
@@ -1,8 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import axios from "axios";
-import { mainButton } from "@telegram-apps/sdk";
-import { secondaryButton } from '@telegram-apps/sdk';
+
 
 
 export default function ProtocolPage() {
@@ -20,59 +19,62 @@ export default function ProtocolPage() {
         }
 
         // Монтируем mainButton, если доступен
-        if (mainButton?.mount?.isAvailable?.()) {
-            mainButton.mount();
-            console.log("mainButton mounted:", mainButton.isMounted?.());
-        }
-
-        // Устанавливаем параметры mainButton, если доступно
-        if (mainButton?.setParams?.isAvailable?.()) {
-            mainButton.setParams({
-                backgroundColor: "#000000",
-                hasShineEffect: true,
-                isEnabled: true,
-                isLoaderVisible: true,
-                isVisible: true,
-                text: "VLESS",
-                textColor: "#ffffff",
-            });
-            console.log("mainButton state:", mainButton.state?.());
-        }
+        // if (mainButton?.mount?.isAvailable?.()) {
+        //     mainButton.mount();
+        //     console.log("mainButton mounted:", mainButton.isMounted?.());
+        // }
+        //
+        // // Устанавливаем параметры mainButton, если доступно
+        // if (mainButton?.setParams?.isAvailable?.()) {
+        //     mainButton.setParams({
+        //         backgroundColor: "#000000",
+        //         hasShineEffect: true,
+        //         isEnabled: true,
+        //         isLoaderVisible: true,
+        //         isVisible: true,
+        //         text: "VLESS",
+        //         textColor: "#ffffff",
+        //     });
+        //     console.log("mainButton state:", mainButton.state?.());
+        // }
 
         // mainButton
 
         // Настраиваем SecondaryButton, если он существует
-        if (secondaryButton.mount.isAvailable()) {
-            secondaryButton.mount();
-            secondaryButton.isMounted(); // true
-        }
-
-        if (secondaryButton.setParams.isAvailable()) {
-            secondaryButton.setParams({
-                backgroundColor: '#000000',
-                hasShineEffect: true,
-                isEnabled: true,
-                isLoaderVisible: true,
-                isVisible: true,
-                position: 'left',
-                text: 'Outline',
-                textColor: '#ffffff'
-            });
-
-            secondaryButton.onClick(() => {
-                console.log("Подключение к Outline VPN");
-                getLinkRedirectOutline(`${site}/connect/run`);
-            });
-        }
+        // if (secondaryButton.mount.isAvailable()) {
+        //     secondaryButton.mount();
+        //     secondaryButton.isMounted(); // true
+        // }
+        //
+        // if (secondaryButton.setParams.isAvailable()) {
+        //     secondaryButton.setParams({
+        //         backgroundColor: '#000000',
+        //         hasShineEffect: true,
+        //         isEnabled: true,
+        //         isLoaderVisible: true,
+        //         isVisible: true,
+        //         position: 'left',
+        //         text: 'Outline',
+        //         textColor: '#ffffff'
+        //     });
+        //
+        tg.secondaryButton.onClick(() => {
+            console.log("Подключение к Outline VPN");
+            getLinkRedirectOutline(`${site}/connect/run`);
+        });
+        tg.secondaryButton.show();
+        // }
 
         // Настраиваем MainButton
-        // tg.MainButton.setText("VLESS");
-        // tg.MainButton.show();
-        //
-        // tg.MainButton.onClick(() => {
-        //     console.log("Подключение к VLESS");
-        //     navigate("/vless");
-        // });
+        tg.MainButton.setText("VLESS");
+        tg.MainButton.hasShineEffect(true);
+        tg.MainButton.show();
+
+        tg.MainButton.onClick(() => {
+            console.log("Подключение к VLESS");
+            navigate("/vless");
+        });
+
 
         // Настраиваем BackButton
         tg.BackButton.show();
@@ -121,13 +123,17 @@ export default function ProtocolPage() {
         <div className="container">
             <h1>Выберите протокол VPN</h1>
             <p>
-                Outline. Прост и легок в подключении. Однако расходует аккумулятор и легче
-                заблокировать со стороны провайдера.
+                VLESS reality (tcp). Это современный протокол особенностью которого является маскировка
+                под обычный интернет трафик. Из-за данной особенности его сложнее заблокировать.
+                Есть возможность добавлять сайты, домены в исключения VPN. Есть роутинг.
+                Сложнее заблокировать операторами связи. Рекомендуем использовать данный протокол.
             </p>
             <p>
-                VLESS. Есть возможность добавлять сайты, домены в исключения VPN. Есть
-                роутинг. Сложнее заблокировать операторами связи. Стоит использовать, если
-                Outline не устраивает или не работает
+                {/*VLESS. Есть возможность добавлять сайты, домены в исключения VPN. Есть*/}
+                {/*роутинг. Сложнее заблокировать операторами связи. Стоит использовать, если*/}
+                {/*Outline не устраивает или не работает*/}
+                Outline. Прост и легок в подключении. Однако расходует аккумулятор, легче заблокировать операторами связи.
+                Рекомендуем использовать его как запасной вариант подключения
             </p>
         </div>
     );
